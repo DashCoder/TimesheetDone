bb14e0d1cd1560a42e0819d0b5146f54f886a537
Axel Sunnehall <axels@dashup.co>
2020-04-21 20:41:07 +0100

    Timesheet app file w. facial recognition,stopwatch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+/* Decription: Main timesheet app with facial recognition */
+/* Future improvements: Nested list. Multiple users have multiple timesheet data */
+
 import React, { useState, useEffect } from 'react'; 
+import { observer } from "mobx-react";
 import logo from './hourglass-personaluseok.png';
-import './App.css';
 import ActivityForm from './ActivityForm'
 import ActivityCard from './ActivityCard'
-import TimeSummary from './TimeSummary'
+import IdentityForm from './IdentifyUser'
+import UserSelectForm from './UserSelectForm'
+import Stopwatch from './Stopwatch'
+import './App.css';
 const uuidv1 = require('uuid/v4');
 
+const App = observer(props => {
 
-const App = () => {
-
+  const appState = props.appState;
   let initialState = () => JSON.parse(localStorage.getItem('timesheetdata')) || [];
   const [state, setState] = useState(initialState);
   const [data, setData] = useState({});
 
+  /* Adding of new timesheet row(s) */
   const handleChange = val => {
     val.id = uuidv1();
+    val.contractorName = appState.userName;
     let newVal = [...state, val];
     setState(newVal);
+  };
+
+  const handleChangeUser = val => {
+    appState.userName = val.contractorName;
+  };
+ 
+  const idChangeHandler = (val) => {
+    /* alert('Facial recognition for ' + JSON.stringify(val) ); */
+    appState.userName = val;
   };
 
   const handleDelete = id => {
@@ -27,8 +44,9 @@ const App = () => {
     setState(newState);
   };
 
-  // Every time rendered with state changed, update graph data
+  /* Every time rendered with state changed, update e.g. graph data */
   useEffect(() => {
+
     localStorage.setItem('timesheetdata', JSON.stringify(state));
     
     let summaryTotal = 0;
@@ -37,15 +55,17 @@ const App = () => {
        item.activityTotal = summaryTotal; // Set to total
     });
 
-
+    /* Iterate through all and do transformations */
+    const contractorName = state.map(obj => obj.contractorName);
     const date = state.map(obj => obj.date.toLocaleString());
     const activityLabel = state.map(obj => obj.activityLabel);
     const activityDuration = state.map(obj => obj.activityTotal); 
-    let newData = { date, activityLabel, activityDuration };
+    let newData = { date, activityLabel, activityDuration, contractorName};
     setData(newData);
+
   }, [state]);
 
-  // Every time component mounted/unmounted
+  /* Every time component mounted/unmounted */
   useEffect(() => {
      console.log('start component');
   }, []);
@@ -55,12 +75,16 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <IdentityForm idChangeHandler={idChangeHandler} appState={appState} />
+        
+        <UserSelectForm appState={appState} />
+        <Stopwatch change={handleChange} />
+  
         <p>
-          Remember! Add activities to timesheet every hour.
+         Remember! Camera identification works best with Firefox.
         </p>
-
-   
-        <ActivityForm change={handleChange} />
+         
+        <ActivityForm change={handleChange} appState={appState} />
 
         <div className='row TileBox'>
             {state.length > 0 ? (
@@ -71,6 +95,7 @@ const App = () => {
                       activityDuration={info.activityDuration}
                       id={info.id}
                       date={info.date}
+                      contractorName={info.contractorName}
                       deleteCard={handleDelete}
                     />
                   ))}
@@ -86,11 +111,12 @@ const App = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          GitLab Project
+          Link to GitLab Project 
         </a>
+
       </header>
     </div>
   );
-}
+}); /* observer ends here */
 
 export default App;
